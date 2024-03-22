0550e79e5808ca5801558e3592ca5c21a781d3ec
Emmanuel <3ma195@gmail.com>
2024-03-22 01:12:10 -0400

    Updating main.h

--- main.h
+++ main.h
@@ -1,4 +1,20 @@
-#ifndef C-printf_Header
-#define C-printf_Header
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
 int _printf(const char *format, ...);
+
+void print_buffer(char buffer[], int *buff_ind);
+
 #endif
